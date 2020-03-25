1b7ece4ec4433a8c2873ebe0bdf64f0341f172e1
Aleksandr Kravchenko <a.kravchenko.intek@gmail.com>
2020-03-25 11:19:11 +0100

    animation

--- Omnifood/resources/js/script.js
+++ Omnifood/resources/js/script.js
@@ -1,1 +1,61 @@
-$(document).ready(function() {});
+$(document).ready(function() {
+  //sticky nav-bar (extension)
+  $('.js--section-features').waypoint(
+    function(direction) {
+      if (direction == 'down') {
+        $('nav').addClass('sticky');
+      } else {
+        $('nav').removeClass('sticky');
+      }
+    },
+    {
+      offset: '60px;'
+    }
+  );
+
+  // Header buttons scroll
+  $('.js--scroll-to-plan').click(function() {
+    $('html, body').animate({ scrollTop: $('.js--section-plans').offset().top }, 1000);
+  });
+
+  $('.js--scroll-to-start').click(function() {
+    $('html, body').animate({ scrollTop: $('.js--section-features').offset().top }, 1000);
+  });
+
+  // Animations on scroll
+  $('.js--wp-1').waypoint(
+    function(direction) {
+      $('.js--wp-1').addClass('animated fadeIn');
+    },
+    {
+      offset: '50%'
+    }
+  );
+
+  $('.js--wp-2').waypoint(
+    function(direction) {
+      $('.js--wp-2').addClass('animated fadeInUp');
+    },
+    {
+      offset: '50%'
+    }
+  );
+
+  $('.js--wp-3').waypoint(
+    function(direction) {
+      $('.js--wp-3').addClass('animated fadeIn');
+    },
+    {
+      offset: '50%'
+    }
+  );
+
+  $('.js--wp-1').waypoint(
+    function(direction) {
+      $('.js--wp-2').addClass('animated pulse');
+    },
+    {
+      offset: '50%'
+    }
+  );
+});
